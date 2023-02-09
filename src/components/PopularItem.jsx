4c7321488401b993311c6e727f5e253ff3b9da4e
Alexander Vetrov <notescape262@gmail.com>
2023-02-10 00:14:38 +0200

    add some styles for popular

--- src/components/PopularItem.jsx
+++ src/components/PopularItem.jsx
@@ -13,6 +13,7 @@ const PopularItem = () => {
       <a className="popular-item__content" href="#">
         <img src="./images/items/backpack.png" alt="backpack" />
         <h5>Водонепроницаемый рюкзак</h5>
+				<span>посмотреть товар</span>
       </a>
       <div className="popular-item__bottom">
         <span className="popular-item__bottom-cost">9 800 ₽</span>
