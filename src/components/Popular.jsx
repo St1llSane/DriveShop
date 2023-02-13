12b20b8698775e362da5d176792221bad0c35ff6
Alexander Vetrov <notescape262@gmail.com>
2023-02-13 18:07:36 +0200

    popular-nav

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,3 +1,4 @@
+import PopularNav from './UI/PopularNav'
 import PopularSlider from './PopularSlider'
 import '../styles/c_styles/popular.scss'
 
@@ -6,48 +7,7 @@ const Popular = () => {
     <section className="popular">
       <div className="popular__top">
         <h3 className="popular__top-title">Популярные товары</h3>
-        <ul className="popular__top-categories">
-          <li>
-            <a href="$" data-nav-color="blue">
-              Катера
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="blue">
-              Гидроциклы
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="blue">
-              Лодки
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="red">
-              Вездеходы
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="red">
-              Квадроциклы
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="white">
-              Снегоходы
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="brown">
-              Двигатели
-            </a>
-          </li>
-          <li>
-            <a href="$" data-nav-color="brown">
-              Запчасти
-            </a>
-          </li>
-        </ul>
+        <PopularNav />
       </div>
       <PopularSlider />
       <a className="popular__more" href="#">
---
+++ src/components/UI/PopularNav.jsx
@@ -0,0 +1,40 @@
+import '../../styles/c_styles/ui_styles/popular-nav.scss'
+
+const PopularNav = () => {
+  return (
+    <ul className="popular-nav">
+      <li>
+        <a href="$" data-nav-color="brown">
+          Запчасти
+        </a>
+      </li>
+      <li>
+        <a href="$" data-nav-color="brown">
+          Моторы
+        </a>
+      </li>
+      <li>
+        <a href="$" data-nav-color="brown">
+          Шины
+        </a>
+      </li>
+      <li>
+        <a href="$" data-nav-color="blue">
+          Электроника
+        </a>
+      </li>
+      <li>
+        <a href="$" data-nav-color="red">
+          Инструменты
+        </a>
+      </li>
+      <li>
+        <a href="$" data-nav-color="white">
+          Аксессуары
+        </a>
+      </li>
+    </ul>
+  )
+}
+
+export default PopularNav
