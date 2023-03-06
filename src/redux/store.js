5b45dc6b97b6f99e7b27a753057ca428e9f82b8d
Alexander Vetrov <notescape262@gmail.com>
2023-03-06 16:36:49 +0200

    added redux persistor and fixed id's for products

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
 import picturesSliderSlice from './slices/picturesSliderSlice'
 import popularSliderSlice from './slices/popularSliderSlice'
 import stockSlice from './slices/stockSlice'
@@ -8,17 +12,46 @@ import filtersParametersSlice from './slices/filtersParametersSlice'
 import productPageSlice from './slices/productPageSlice'
 import activePageSlice from './slices/activePageSlice'
 import favoriteItemSlice from './slices/favoriteItemSlice'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+  picturesSliderSlice,
+  popularSliderSlice,
+  stockSlice,
+  categoriesSlice,
+  headerFiltersSlice,
+  filtersParametersSlice,
+  activePageSlice,
+  productPageSlice,
+  favoriteItemSlice,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['favoriteItemSlice'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    picturesSliderSlice,
-    popularSliderSlice,
-    stockSlice,
-    categoriesSlice,
-    headerFiltersSlice,
-    filtersParametersSlice,
-    activePageSlice,
-    productPageSlice,
-    favoriteItemSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
