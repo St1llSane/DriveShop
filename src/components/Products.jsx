33eea92521ab76a7c5e29d5e92f605aa9b9c81b3
Alexander Vetrov <notescape262@gmail.com>
2023-03-24 18:14:23 +0200

    added ProductItemSkeleton component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,28 +4,35 @@ import { useSelector } from 'react-redux'
 import { headerFiltersSliceSelector } from '../redux/slices/headerFiltersSlice'
 import { activeProductsGridSelector } from '../redux/slices/activeProductsGridSlice'
 import ProductItem from './ProductItem'
+import ProductItemSkeleton from './UI/ProductItemSkeleton'
 import '../styles/c_styles/products.scss'
 
 const Products = () => {
   const location = useLocation()
   const { activeSort } = useSelector(headerFiltersSliceSelector)
   console.log(activeSort.filter)
-  const { data = [] } = useGetProductsQuery({
+  const { data = [], isLoading } = useGetProductsQuery({
     location: location.pathname,
     filter: activeSort.filter,
   })
   const { activeGridId } = useSelector(activeProductsGridSelector)
 
+  const skeleton = [...new Array(7).keys()].map((key) => (
+    <ProductItemSkeleton key={key} />
+  ))
+
+  const content = data.map((item) => (
+    <ProductItem
+      item={item}
+      activeGrid={activeGridId === 2 ? 'list' : ''}
+      key={item.id}
+    />
+  ))
+
   return (
     <>
       <div className={`products ${activeGridId === 2 ? 'list' : ''}`}>
-        {data.map((item) => (
-          <ProductItem
-            item={item}
-            activeGrid={activeGridId === 2 ? 'list' : ''}
-            key={item.id}
-          />
-        ))}
+        {isLoading ? skeleton : content}
       </div>
     </>
   )
---
+++ src/components/UI/ProductItemSkeleton.jsx
@@ -0,0 +1,19 @@
+import '../../styles/c_styles/ui_styles/product-Item-skeleton.scss'
+
+const ProductItemSkeleton = () => {
+  return (
+    <div className="product-Item-skeleton">
+      <div className="product-Item-skeleton__top skeleton"></div>
+      <div className="product-Item-skeleton__middle">
+        <div className="product-Item-skeleton__middle-img skeleton"></div>
+        <div className="product-Item-skeleton__middle-text skeleton"></div>
+      </div>
+      <div className="product-Item-skeleton__bottom">
+        <div className="product-Item-skeleton__bottom-text skeleton"></div>
+        <div className="product-Item-skeleton__bottom-cart skeleton"></div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductItemSkeleton
